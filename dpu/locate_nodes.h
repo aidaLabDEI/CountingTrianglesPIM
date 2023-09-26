7627457d5eb1bc84c5b12f7325d41456f6c674c9
LorenzoAsquini <lorenzo.asquini@studenti.unipd.it>
2023-09-26 18:37:50 +0200

    Performance improvements

--- dpu/locate_nodes.h
+++ dpu/locate_nodes.h
@@ -21,7 +21,7 @@ uint32_t node_locations(__mram_ptr edge_t* sample, uint32_t edges_in_sample, __m
 void write_nodes_loc(uint32_t* nodes_loc_buffer_index, node_loc_t* nodes_loc_buffer_wram, __mram_ptr node_loc_t* AFTER_SAMPLE_HEAP_POINTER, bool last_write);
 
 //Read new edges from the sample
-uint32_t read_from_sample(__mram_ptr edge_t* sample, uint32_t edges_in_sample, edge_t* sample_buffer_wram, uint32_t* previous_node_id, uint32_t* edges_read);
+uint32_t virtually_read_from_sample(uint32_t edges_in_sample, uint32_t* edges_read);
 
 //Debug function to know the information about the locations of the unique nodes
 void print_node_locations(uint32_t number_of_nodes, __mram_ptr void* AFTER_SAMPLE_HEAP_POINTER);
