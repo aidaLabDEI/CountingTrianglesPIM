a69da3b0f4abd5e57937c1cdb05cc295ea9e5316
lorenzo-asquini <asqlorenzo@gmail.com>
2023-11-02 11:24:40 +0100

    Update app.c

--- host/app.c
+++ host/app.c
@@ -260,7 +260,7 @@ int main(int argc, char* argv[]){
     float triangle_counting_time = timedifference_msec(start, now);
     printf("Time to count the triangles: %f\n", triangle_counting_time);
 
-    printf("Triangles: %ld", total_triangle_estimation);
+    printf("Triangles: %ld\n", total_triangle_estimation);
 
     //For debug purpose, get standard output from the DPUs
     /*DPU_FOREACH(dpu_set, dpu) {
